e1da08d332af05330039fd99e1c3f7137586aa73
Youngjun Kim <neosado@gmail.com>
2016-11-10 12:13:30 -0800

    update

--- rtest.jl
+++ rtest.jl
@@ -1,4 +1,60 @@
-addprocs(CPU_CORES - 2)
+function init_cluster_sherlock()
+
+    if !haskey(ENV, "SLURM_JOB_NODELIST")
+        error("SLURM_JOB_NODELIST not defined")
+    end
+
+    if !haskey(ENV, "SLURM_NTASKS_PER_NODE")
+        error("SLURM_NTASKS_PER_NODE not defined")
+    end
+
+    n = parse(Int64, ENV["SLURM_NTASKS_PER_NODE"])
+    dir = "/home/youngjun/MultiArmedBandit"
+
+    machines = Tuple{ASCIIString, Int64}[]
+
+    list = ENV["SLURM_JOB_NODELIST"]
+
+    for m = eachmatch(r"([\w\d-]+)(\[[\d,-]+\])?", list)
+        if m.captures[2] == nothing
+            push!(machines, (m.captures[1], n))
+
+        else
+            host_pre = m.captures[1]
+
+            s = split(m.captures[2][2:end-1], ",")
+            for s_ in s
+                if isdigit(s_)
+                    push!(machines, (host_pre * s_, n))
+                else
+                    a, b = split(s_, "-")
+                    for i = parse(Int64, a):parse(Int64, b)
+                        push!(machines, (host_pre * string(i), n))
+                    end
+                end
+            end
+
+        end
+    end
+
+    for (machine, count) in machines
+        cluster_list = ASCIIString[]
+
+        for i = 1:count
+            push!(cluster_list, machine)
+        end
+
+        addprocs(cluster_list, dir = dir)
+    end
+end
+
+
+if contains(gethostname(), "sherlock")
+    init_cluster_sherlock()
+else
+    addprocs(CPU_CORES - 2)
+end
+
 
 using JLD
 
